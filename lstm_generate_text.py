a3f5ef862a912bebbb35a0f8ddf877ae9fe23f5d
Sofian Audry <info@sofianaudry.com>
2018-06-04 22:25:01 +0200

    Updated lstm_generate_text to a working version (loads models directly, not weights).

--- lstm_generate_text.py
+++ lstm_generate_text.py
@@ -7,6 +7,7 @@ parser.add_argument("model_file", type=str, help="The file containing the traine
 parser.add_argument("-n", "--n-hidden", type=int, default=256, help="Number of hidden units per layer")
 parser.add_argument("-l", "--n-layers", type=int, default=1, help="Number of layers")
 parser.add_argument("-s", "--sequence-length", type=int, default=100, help="Sequence length")
+parser.add_argument("-em", "--embedding-length", type=int, default=0, help="Size of vector to use for first layer embedding (if 0 : don't use embedding)")
 parser.add_argument("-S", "--sampling-mode", type=str, default="argmax", choices=["argmax", "softmax", "special"], help="Sampling policy")
 parser.add_argument("-N", "--n-words", type=int, default=1000, help="Number of words to generate")
 parser.add_argument("-T", "--temperature", type=float, default=1, help="Temperature argument [0, +inf] (for softmax sampling) (higher: more uniform, lower: more greedy")
@@ -15,7 +16,7 @@ args = parser.parse_args()
 
 import sys
 import numpy
-from keras.models import Sequential
+from keras.models import Sequential, Model, load_model
 from keras.layers import Dense
 from keras.layers import Dropout
 from keras.layers import LSTM
@@ -34,8 +35,8 @@ int_to_char = dict((i, c) for i, c in enumerate(chars))
 # summarize the loaded data
 n_chars = len(raw_text)
 n_vocab = len(chars)
-print "Total Characters: ", n_chars
-print "Total Vocab: ", n_vocab
+print("Total Characters: ", n_chars)
+print("Total Vocab: ", n_vocab)
 
 # prepare the dataset of input to output pairs encoded as integers
 seq_length = args.sequence_length
@@ -47,53 +48,71 @@ for i in range(0, n_chars - seq_length, 1):
 	dataX.append([char_to_int[char] for char in seq_in])
 	dataY.append(char_to_int[seq_out])
 n_patterns = len(dataX)
-print "Total Patterns: ", n_patterns
-# reshape X to be [samples, time steps, features]
-X = numpy.reshape(dataX, (n_patterns, seq_length, 1))
-# normalize
-X = X / float(n_vocab)
+print("Total Patterns: ", n_patterns)
+
+
+dataX = numpy.array(dataX)
+dataY = numpy.array(dataY)
+
 # one hot encode the output variable
 y = np_utils.to_categorical(dataY)
 
+if args.embedding_length <= 0:
+	# reshape X to be [samples, time steps, features] and normalize
+	X = numpy.reshape(dataX, (n_patterns, seq_length, 1)) / float(n_vocab)
+else:
+	# reshape X to be [samples, time steps]
+	X = numpy.reshape(dataX, (n_patterns, seq_length))
+
+n_words = args.n_words
 
 # define the LSTM model
-model = Sequential()
-model.add(LSTM(args.n_hidden, input_shape=(X.shape[1], X.shape[2]), return_sequences=(args.n_layers > 1)))
-model.add(Dropout(0.2))
-for l in range(1, args.n_layers):
-	model.add(LSTM(args.n_hidden, return_sequences=(l < args.n_layers-1)))
-	model.add(Dropout(0.2))
-model.add(Dense(y.shape[1], activation='softmax'))
+model = load_model(args.model_file)
+# model = Sequential()
+# model.add(LSTM(args.n_hidden, input_shape=(X.shape[1], X.shape[2]), return_sequences=(args.n_layers > 1)))
+# model.add(Dropout(0.2))
+# for l in range(1, args.n_layers):
+# 	model.add(LSTM(args.n_hidden, return_sequences=(l < args.n_layers-1)))
+# 	model.add(Dropout(0.2))
+# model.add(Dense(y.shape[1], activation='softmax'))
 
-print model.summary()
+print((model.summary()))
 
-# load the network weights
-model.load_weights(args.model_file)
-model.compile(loss='categorical_crossentropy', optimizer='adam')
+# # load the network weights
+# model.load_weights(args.model_file)
+# model.compile(loss='categorical_crossentropy', optimizer='adam')
 
 # pick a random seed
 start = numpy.random.randint(0, len(dataX)-1)
 pattern = dataX[start]
 
-print "Seed:"
-print "\"", ''.join([int_to_char[value] for value in pattern]), "\""
+print("Seed:")
+print(("\"", ''.join([int_to_char[value] for value in pattern]), "\""))
 
 temperature = args.temperature
 
 # generate characters
-for i in range(args.n_words):
-	x = numpy.reshape(pattern, (1, len(pattern), 1))
-	x = x / float(n_vocab)
-	prediction = model.predict(x, verbose=0)
+for i in range(n_words):
+	if args.embedding_length <= 0:
+		x = numpy.reshape(pattern, (1, len(pattern), 1)) / float(n_vocab)
+	else:
+		x = numpy.reshape(pattern, (1, len(pattern)))
+
+	# run prediction of model
+	prediction = model.predict(x, verbose=0).squeeze()
+
+	# if transition_factor > 0 and i >= transition_start_word:
+	#   prediction_next = model_next.predict(x, verbose=0).squeeze()
+	#   mix_factor = (n_words - i) / float(transition_n_words)
+	#   prediction = mix_factor*prediction + (1-mix_factor)*prediction_next
 
 	# argmax
 	if (args.sampling_mode == "argmax"):
-		index = numpy.argmax(prediction.squeeze())
+		index = numpy.argmax(prediction)
 
 	# random
-	else:
+	elif (args.sampling_mode == "softmax"):
 		# Source: https://gist.github.com/alceufc/f3fd0cd7d9efb120195c
-		prediction = prediction.squeeze()
 		if (temperature != 1):
 			prediction = numpy.power(prediction, 1./temperature)
 			prediction /= prediction.sum(0)
@@ -101,14 +120,14 @@ for i in range(args.n_words):
 		#print "[" + args.sampling_mode +"]"
 		#print (args.sampling_mode == "softmax")
 
-		if (args.sampling_mode == "softmax"):
+		if n_best == 0:
 			#print "using softmax"
 			index = numpy.asscalar(numpy.random.choice(numpy.arange(n_vocab), 1, p=prediction))
 
 		# special
 		else:
 			prediction = numpy.asarray(prediction)
-			max_indices = prediction.argsort()[-3:][::-1]
+			max_indices = prediction.argsort()[-n_best:][::-1]
 			max_indices_weights = [ prediction[m] for m in max_indices ]
 			max_indices_sum = numpy.sum(max_indices_weights)
 			max_indices_weights = [ prediction[m]/max_indices_sum for m in max_indices ]
@@ -117,8 +136,7 @@ for i in range(args.n_words):
 	result = int_to_char[index]
 	seq_in = [int_to_char[value] for value in pattern]
 	sys.stdout.write(result)
-	pattern.append(index)
+	pattern = numpy.append(pattern, index)
+#		pattern.append(index)
 	pattern = pattern[1:len(pattern)]
-
-print "\nDone."
-1
+print("\nDone.")
