89f77da681437b0b325216dc802d8105b6077732
Sofian Audry <info@sofianaudry.com>
2018-09-06 15:52:07 -0400

    Updated script to transform weights into model files.

--- lstm_weights2model.py
+++ lstm_weights2model.py
@@ -1,17 +1,14 @@
 # Load LSTM network and generate text
 import argparse
+import io
 
 parser = argparse.ArgumentParser()
 parser.add_argument("text_file", type=str, help="The file containing the original text")
 parser.add_argument("model_weights_file", type=str, help="The file containing the trained model weights")
 parser.add_argument("model_output_file", type=str, help="The output file where to store the trained model")
-parser.add_argument("-n", "--n-hidden", type=int, default=256, help="Number of hidden units per layer")
-parser.add_argument("-l", "--n-layers", type=int, default=1, help="Number of layers")
-parser.add_argument("-s", "--sequence-length", type=int, default=100, help="Sequence length")
+parser.add_argument("-n", "--n-hidden", type=str, default="256", help="Number of hidden units per layer, as a comma-separated list")
+parser.add_argument("-d", "--dropout", type=str, default="0.2", help="Dropout per layer, as a comma-separated list")
 parser.add_argument("-em", "--embedding-length", type=int, default=0, help="Size of vector to use for first layer embedding (if 0 : don't use embedding)")
-parser.add_argument("-S", "--sampling-mode", type=str, default="argmax", choices=["argmax", "softmax", "special"], help="Sampling policy")
-parser.add_argument("-N", "--n-words", type=int, default=1000, help="Number of words to generate")
-parser.add_argument("-T", "--temperature", type=float, default=1, help="Temperature argument [0, +inf] (for softmax sampling) (higher: more uniform, lower: more greedy")
 
 args = parser.parse_args()
 
@@ -24,23 +21,29 @@ from keras.utils import np_utils
 
 
 def create_model():
-	global args, X, n_vocab, seq_length
+	global args, X, n_vocab, seq_length, n_layers
 
 	# define the LSTM model
 	model = Sequential()
 
+	n_hidden = args.n_hidden.split(',')
+	dropout = args.dropout.split(',')
+	n_layers = len(n_hidden)
+	if (n_layers != len(dropout)):
+		sys.exit("Length of --n-hidden and --dropout do not match.")
+
 	if args.embedding_length <= 0:
 		# add first LSTM layer
-		model.add(LSTM(args.n_hidden, input_shape=(X.shape[1], X.shape[2]), return_sequences=(args.n_layers > 1)))
+		model.add(LSTM(int(n_hidden[0]), input_shape=(X.shape[1], X.shape[2]), return_sequences=(n_layers > 1)))
 	else:
 		# add embedded layer + LSTM layer
 		model.add(Embedding(n_vocab, args.embedding_length, input_length=seq_length))
-		model.add(LSTM(args.n_hidden, return_sequences=(args.n_layers > 1)))
+		model.add(LSTM(int(n_hidden[0]), return_sequences=(n_layers > 1)))
 
-	model.add(Dropout(0.2))
-	for l in range(1, args.n_layers):
-	  model.add(LSTM(args.n_hidden, return_sequences=(l < args.n_layers-1)))
-	  model.add(Dropout(0.2))
+	model.add(Dropout(float(dropout[0])))
+	for l in range(1, n_layers):
+	  model.add(LSTM(int(n_hidden[l]), return_sequences=(l < n_layers-1)))
+	  model.add(Dropout(float(dropout[l])))
 	model.add(Dense(y.shape[1], activation='softmax'))
 	return model
 
@@ -49,10 +52,10 @@ def load_model(model, model_file):
 	model.load_weights(model_file)
 	model.compile(loss='categorical_crossentropy', optimizer='adam')
 
-# load ascii text and covert to lowercase
-raw_text = open(args.text_file).read()
+# load text and covert to lowercase
+with io.open(args.text_file, "r", encoding="utf-8") as f:
+	raw_text = f.read()
 raw_text = raw_text.lower()
-
 # create mapping of unique chars to integers, and a reverse mapping
 chars = sorted(list(set(raw_text)))
 char_to_int = dict((c, i) for i, c in enumerate(chars))
@@ -76,7 +79,7 @@ for i in range(0, n_chars - seq_length, 1):
 n_patterns = len(dataX)
 print("Total Patterns: ", n_patterns)
 
-dataX = numpy.array(dataX)
+adataX = numpy.array(dataX)
 dataY = numpy.array(dataY)
 
 # one hot encode the output variable
@@ -90,6 +93,7 @@ else:
 	X = numpy.reshape(dataX, (n_patterns, seq_length))
 
 model = create_model()
+print(model.summary)
 load_model(model, args.model_weights_file)
 print(model.summary())
 
