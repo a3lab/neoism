4fb630c5e1d9aacbbe618da0a07d286ffcc8a28a
Sofian Audry <info@sofianaudry.com>
2018-09-16 15:08:37 -0400

    Startup PC script.

---
+++ neoism_startup_pc.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+export PATH="/home/sofian/anaconda3/bin:$PATH"
+source activate tf
+cd /home/sofian/Documents/neoism
+python3 lstm_generate_brainfuck.py -I 192.168.1.110 -sP 7770 -S softmax -T 0.5 --arduino-serial-number 7573530323135161F1D2 --frame-rate 10 data/neoism.txt neoism_l2_n64,1024_d0.1,0.2_em5_b512/lstm-weights--layers2-nhu64,1024-e054.model.hdf5
