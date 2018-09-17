d68e7775d1011cc46b122b1ec64e58f4763c0b66
Sofian Audry <info@sofianaudry.com>
2018-09-16 23:01:13 -0400

    Adjust x and y offsets + other improvements on startup script on Pi

--- neoism_startup_pi.sh
+++ neoism_startup_pi.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /home/pi/neoism/led_display
-sudo ./neoism-scrolling-text  --led-gpio-mapping=adafruit-hat-pwm --led-rows=16 --led-cols=32 --led-chain=2 --led-brightness=80  --led-pwm-lsb-nanoseconds=250 -s 5 -f "./fonts/9x18B.bdf" --led-show-refresh --led-slowdown-gpio=2 -C 128,128,128 --led-scan-mode=0
+sudo ./neoism-scrolling-text  --led-gpio-mapping=adafruit-hat-pwm --led-rows=16 --led-cols=32 --led-chain=2 --led-brightness=80 --led-pwm-lsb-nanoseconds=500 -x 1 -y -1 -s 5 -f "./fonts/9x18B.bdf" --led-slowdown-gpio=2 -C 254,254,1 --led-scan-mode=1
 
