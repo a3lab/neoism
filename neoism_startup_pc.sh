8b7d4d49dc17fbc6836a4fbcbb43612771638b05
Sofian Audry <info@sofianaudry.com>
2021-05-07 16:40:37 -0400

    Added final model in the git archive.

--- neoism_startup_pc.sh
+++ neoism_startup_pc.sh
@@ -2,4 +2,4 @@
 export PATH="$HOME/anaconda3/bin:$PATH"
 source activate neoism
 cd $HOME/neoism
-python3 lstm_generate_brainfuck.py -I 192.168.1.110 -sP 7770 -S softmax --arduino-serial-number 7573530323135161F1D2 data/neoism.txt neoism_l2_n64,1024_d0.1,0.2_em5_b512/lstm-weights--layers2-nhu64,1024-e054.model.hdf5 > /home/tats/neoism/neoism.log 2> /home/tats/neoism/neoism_err.log
+python3 lstm_generate_brainfuck.py -I 192.168.1.110 -sP 7770 -S softmax --arduino-serial-number 7573530323135161F1D2 data/neoism.txt neoism_l2_n64,1024_d0.1,0.2_em5_b512-final.model.hdf5 > /home/tats/neoism/neoism.log 2> /home/tats/neoism/neoism_err.log
